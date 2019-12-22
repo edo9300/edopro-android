ac8851db2c16a5054aa7f3946e4982b85f2c26c0
edo9300 <edoardo762@gmail.com>
2019-12-22 12:32:54 +0100

    Updated freetype to 2.10

--- sources/freetype_src/Android.mk
+++ sources/freetype_src/Android.mk
@@ -1,36 +1,51 @@
 # this is now the default FreeType build for Android
 #
 ifndef USE_FREETYPE
-USE_FREETYPE := 2.4.2
+USE_FREETYPE := 2.10.0
 endif
 
-ifeq ($(USE_FREETYPE),2.4.2)
+ifeq ($(USE_FREETYPE),2.10.0)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 # compile in ARM mode, since the glyph loader/renderer is a hotspot
 # when loading complex pages in the browser
 #
-LOCAL_ARM_MODE := arm
+#LOCAL_ARM_MODE := arm
 
 LOCAL_SRC_FILES:= \
-	src/base/ftbbox.c \
-	src/base/ftbitmap.c \
-	src/base/ftglyph.c \
-	src/base/ftstroke.c \
-	src/base/ftxf86.c \
-	src/base/ftbase.c \
-	src/base/ftsystem.c \
-	src/base/ftinit.c \
-	src/base/ftgasp.c \
-	src/raster/raster.c \
-	src/sfnt/sfnt.c \
-	src/smooth/smooth.c \
-	src/autofit/autofit.c \
-	src/truetype/truetype.c \
-	src/cff/cff.c \
-	src/psnames/psnames.c \
-	src/pshinter/pshinter.c
+    src/autofit/autofit.c \
+    src/base/ftbase.c \
+    src/base/ftbbox.c \
+    src/base/ftbitmap.c \
+    src/base/ftdebug.c \
+    src/base/ftgasp.c \
+    src/base/ftglyph.c \
+    src/base/ftinit.c \
+    src/base/ftlcdfil.c \
+    src/base/ftmm.c \
+    src/base/ftfntfmt.c \
+    src/base/ftpatent.c \
+    src/base/ftsynth.c \
+    src/base/ftstroke.c \
+    src/base/ftsystem.c \
+    src/bdf/bdf.c \
+    src/cff/cff.c \
+    src/cid/type1cid.c \
+    src/gzip/ftgzip.c \
+    src/lzw/ftlzw.c \
+    src/pcf/pcf.c \
+    src/pfr/pfr.c \
+    src/psaux/psaux.c \
+    src/pshinter/pshinter.c \
+    src/psnames/psnames.c \
+    src/raster/raster.c \
+    src/sfnt/sfnt.c \
+    src/smooth/smooth.c \
+    src/truetype/truetype.c \
+    src/type1/type1.c \
+    src/type42/type42.c \
+    src/winfonts/winfnt.c
 
 LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH)/builds \
@@ -38,21 +53,7 @@ LOCAL_C_INCLUDES += \
 
 LOCAL_CFLAGS += -W -Wall
 LOCAL_CFLAGS += -fPIC -DPIC
-LOCAL_CFLAGS += "-DDARWIN_NO_CARBON"
-LOCAL_CFLAGS += "-DFT2_BUILD_LIBRARY"
-
-# enable the FreeType internal memory debugger in the simulator
-# you need to define the FT2_DEBUG_MEMORY environment variable
-# when running the program to activate it. It will dump memory
-# statistics when FT_Done_FreeType is called
-#
-ifeq ($(TARGET_SIMULATOR),true)
-LOCAL_CFLAGS += "-DFT_DEBUG_MEMORY"
-endif
-
-# the following is for testing only, and should not be used in final builds
-# of the product
-#LOCAL_CFLAGS += "-DTT_CONFIG_OPTION_BYTECODE_INTERPRETER"
+LOCAL_CFLAGS += -DFT2_BUILD_LIBRARY
 
 LOCAL_CFLAGS += -O2
 
