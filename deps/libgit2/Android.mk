a669e690c6ab96abd9e4b7307528793aa6b5e905
edo9300 <edoardo762@gmail.com>
2020-01-08 21:02:34 +0100

    Remove no longer needed libssh2 and openssl from deps folder

--- deps/libgit2/Android.mk
+++ deps/libgit2/Android.mk
@@ -3,7 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := git2
-LOCAL_STATIC_LIBRARIES := ssh2
 LOCAL_SRC_FILES := ./lib/$(TARGET_ARCH_ABI)/libgit2.a
 
 include $(PREBUILT_STATIC_LIBRARY)
