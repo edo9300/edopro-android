529ab3a25b189212fb50f3bdaf76505109766f1e
edo9300 <edoardo762@gmail.com>
2020-01-08 21:54:52 +0100

    Fix path in copy script

--- copyocgcore.sh
+++ copyocgcore.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 archs=("armeabi-v7a" "arm64-v8a" "x86" "x86_64" )
 names=("libocgcorev7.so" "libocgcorev8.so" "libocgcorex86.so" "libocgcorex64.so")
-mkdir -p ../src/main/assets/default
+mkdir -p src/main/assets/default
 for i in {0..3}; do
-	FILE="../libs/${archs[i]}/libocgcore.so"
-	DEST="../src/main/assets/default/${names[i]}"
+	FILE="libs/${archs[i]}/libocgcore.so"
+	DEST="src/main/assets/defaults/${names[i]}"
 	if test -f "$FILE"; then
 		yes | cp -rf $FILE $DEST
+		yes | rm -rf $FILE
 	fi
 done
