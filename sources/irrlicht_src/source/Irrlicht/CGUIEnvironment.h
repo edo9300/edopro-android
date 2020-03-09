84cf85e0dbf6015421c7995758a7f5c839572105
edo9300 <edoardo762@gmail.com>
2020-03-09 19:37:16 +0100

    Move setOSOperator to Interface

--- sources/irrlicht_src/source/Irrlicht/CGUIEnvironment.h
+++ sources/irrlicht_src/source/Irrlicht/CGUIEnvironment.h
@@ -42,8 +42,8 @@ public:
 	//! returns a pointer to the OS operator
 	virtual IOSOperator* getOSOperator() const _IRR_OVERRIDE_;
 
-	//! Sets a new GUI Skin
-	virtual void setOSOperator(IOSOperator* Operator);
+	//! Sets a new OS operator
+	virtual void setOSOperator(IOSOperator* Operator) _IRR_OVERRIDE_;
 
 	//! posts an input event to the environment
 	virtual bool postEventFromUser(const SEvent& event) _IRR_OVERRIDE_;
