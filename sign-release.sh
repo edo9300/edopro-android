e4001497944fd537d7857e279e609874ebc232dc
kevinlul <6320810+kevinlul@users.noreply.github.com>
2020-04-26 23:02:53 -0400

    Specify build-tools path

--- sign-release.sh
+++ sign-release.sh
@@ -3,7 +3,8 @@
 set -euo pipefail
 
 JKS_PASS=${1:-JKS_PASS}
+BUILD_TOOLS_PATH=${BUILD_TOOLS_PATH:-$ANDROID_HOME/build-tools/29.0.3}
 
-zipalign -v -p 4 build/outputs/apk/release/EDOPro-release-unsigned.apk build/outputs/apk/release/EDOPro-release-unsigned-aligned.apk
+"$BUILD_TOOLS_PATH"/zipalign -v -p 4 build/outputs/apk/release/EDOPro-release-unsigned.apk build/outputs/apk/release/EDOPro-release-unsigned-aligned.apk
 mkdir -p release
-apksigner sign --ks keys.jks --ks-pass pass:"$JKS_PASS" --out release/EDOPro-release.apk build/outputs/apk/release/EDOPro-release-unsigned-aligned.apk
+"$BUILD_TOOLS_PATH"/apksigner sign --ks keys.jks --ks-pass pass:"$JKS_PASS" --out release/EDOPro-release.apk build/outputs/apk/release/EDOPro-release-unsigned-aligned.apk
