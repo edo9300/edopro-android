693078ede24a0368566d00a7386254906afd77a5
kevinlul <6320810+kevinlul@users.noreply.github.com>
2020-04-26 23:17:52 -0400

    Use envvar JKS_PASS properly

--- sign-release.sh
+++ sign-release.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-JKS_PASS=${1:-JKS_PASS}
+JKS_PASS=${1:-$JKS_PASS}
 BUILD_TOOLS_PATH=${BUILD_TOOLS_PATH:-$ANDROID_HOME/build-tools/29.0.3}
 
 "$BUILD_TOOLS_PATH"/zipalign -v -p 4 build/outputs/apk/release/EDOPro-release-unsigned.apk build/outputs/apk/release/EDOPro-release-unsigned-aligned.apk
