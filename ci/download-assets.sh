914357460ca9c353b317b4f80ee55fc3deb41337
kevinlul <6320810+kevinlul@users.noreply.github.com>
2020-04-30 21:31:40 -0400

    Add German strings and some licenses

--- ci/download-assets.sh
+++ ci/download-assets.sh
@@ -9,6 +9,8 @@ ASSETS=src/main/assets/defaults
 mkdir -p $ASSETS
 
 # TODO: copy code licenses
+curl --retry 5 --connect-timeout 30 --location --remote-header-name --remote-name https://raw.githubusercontent.com/edo9300/edopro/master/COPYING
+curl --retry 5 --connect-timeout 30 --location --remote-header-name --remote-name https://raw.githubusercontent.com/edo9300/edopro/master/LICENSE
 
 git clone https://$DEPLOY_TOKEN@github.com/kevinlul/edopro-config.git
 cd edopro-config
@@ -16,7 +18,7 @@ git submodule sync
 git submodule update --init --recursive --force
 cd ..
 
-rsync -ar --exclude='.*' --exclude=update.sh --exclude='script/.*' \
+rsync -ar --exclude='.*' --exclude='*.sh' --exclude='script/.*' \
 	--exclude='expansions/.*' --exclude='expansions/ci' --exclude='expansions/README.md' \
 	--exclude=textures/Backup --exclude=puzzles \
 	edopro-config/ $ASSETS
@@ -29,6 +31,6 @@ find $ASSETS \( -name '*.md' -o -name 'COPYING*' -o -name 'LICENSE*' \) -exec mv
 mkdir -p $ASSETS/deck
 find $ASSETS/deck ! \( -name 'Starter*' -o -name 'Structure*' \) -type f -exec rm {} \;
 # Unsupported for release
-rm -rf $ASSETS/config/*.json $ASSETS/config/languages/Deutsch $ASSETS/config/languages/Thai
+rm -rf $ASSETS/config/*.json $ASSETS/config/languages/Thai
 cp edopro-config/config/configs.prod.json $ASSETS/config/configs.json
 sed -i 's/dpi_scale = .*/dpi_scale = 2.000000/' $ASSETS/config/system.conf
