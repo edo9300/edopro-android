144d8528cc1bed75e955e675460fc21d929cef6f
edo9300 <edoardo762@gmail.com>
2020-07-27 19:43:34 +0200

    Update vcpkg built libraries
    Update openssl, libcurl, libgit2 (built with ssh support and the android patch). Add their proper headers as well to match the current version of the library

--- deps/libgit2/Android.mk
+++ deps/libgit2/Android.mk
@@ -1,9 +1,15 @@
 LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libssh2
+LOCAL_SRC_FILES := ./lib/$(TARGET_ARCH_ABI)/libssh2.a
+
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := git2
-LOCAL_STATIC_LIBRARIES := libssl libcrypto
+LOCAL_STATIC_LIBRARIES := libssl libcrypto libssh2
 LOCAL_SRC_FILES := ./lib/$(TARGET_ARCH_ABI)/libgit2.a
 
 include $(PREBUILT_STATIC_LIBRARY)
