8a6f0fe30b0e237d18047d63bae99102bb72b578
edo9300 <edoardo762@gmail.com>
2020-11-09 14:33:59 +0100

    Update libevent version 2.1.11-5

--- deps/libevent/Android.mk
+++ deps/libevent/Android.mk
@@ -2,7 +2,15 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_MODULE := libevent_pthreads
+LOCAL_SRC_FILES := ./lib/$(TARGET_ARCH_ABI)/libevent_pthreads.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
 LOCAL_MODULE:= libevent2
-LOCAL_SRC_FILES := ./lib/$(TARGET_ARCH_ABI)/libevent2.a
+LOCAL_STATIC_LIBRARIES := libevent_pthreads
+LOCAL_SRC_FILES := ./lib/$(TARGET_ARCH_ABI)/libevent_core.a
 
 include $(PREBUILT_STATIC_LIBRARY)
