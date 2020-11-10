20c0dec706eb401fd1681600b8f601d38ee11bf3
edo9300 <edoardo762@gmail.com>
2020-11-10 12:59:13 +0100

    [ci skip]

--- deps/SDL2/Android.mk
+++ deps/SDL2/Android.mk
@@ -11,7 +11,7 @@ include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := libhidapi
+LOCAL_MODULE := hidapi
 LOCAL_SRC_FILES := ./lib/$(TARGET_ARCH_ABI)/libhidapi.so
 
 include $(PREBUILT_SHARED_LIBRARY)
