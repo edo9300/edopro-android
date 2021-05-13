3514f3414afb2a2b33a9b000fcb651d89a3d60be
Edoardo Lolletti <edoardo762@gmail.com>
2021-05-13 11:37:59 +0200

    Update setndk.sh

--- setndk.sh
+++ setndk.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export NDK=/mnt/d/0Linux/Scaricati/android-ndk-r20
+export PATH=/mnt/d/0Linux/Scaricati/android-ndk-r20:$PATH
 export ndk_build=$NDK/ndk-build
 export ndk_stack=$NDK/ndk-stack
 export PROJECT_PATH=$PWD
