1f1e8e44be55a15ddf76152eb737ed75c9975c71
Edoardo Lolletti <edoardo762@gmail.com>
2021-07-04 12:06:57 +0200

    Sync

--- deps/gframe/Android/porting_android.cpp
+++ deps/gframe/Android/porting_android.cpp
@@ -77,6 +77,7 @@ jstring NewJavaString(JNIEnv* env, epro::stringview string) {
 }
 
 std::mutex* queued_messages_mutex = nullptr;
+std::atomic_bool error_dialog_returned{ true };
 std::deque<std::function<void()>>* events = nullptr;
 std::unique_ptr<std::unique_lock<std::mutex>> mainGameMutex = nullptr;
 }
@@ -136,6 +137,11 @@ extern "C" {
 			});
 			queued_messages_mutex->unlock();
 		}
+	}
+
+	JNIEXPORT void JNICALL Java_io_github_edo9300_edopro_EpNativeActivity_errorDialogReturn(
+		JNIEnv* env, jclass thiz) {
+		error_dialog_returned = true;
 	}
 }
 
@@ -503,6 +509,31 @@ JAVAVOIDSTRINGMETHOD(installUpdate)
 JAVAVOIDSTRINGMETHOD(openUrl)
 JAVAVOIDSTRINGMETHOD(openFile)
 
+void showErrorDialog(epro::stringview context, epro::stringview message) {
+	jmethodID showDialog = jnienv->GetMethodID(nativeActivity, "showErrorDialog", JPARAMS(JSTRING JSTRING)JVOID);
+	if(showDialog == 0)
+		assert("porting::showErrorDialog unable to find java showErrorDialog method" == 0);
+	jstring jcontext = NewJavaString(jnienv, context);
+	jstring jmessage = NewJavaString(jnienv, message);
+
+	error_dialog_returned = false;
+
+	jnienv->CallVoidMethod(app_global->activity->clazz, showDialog, jcontext, jmessage);
+
+	jnienv->DeleteLocalRef(jcontext);
+	jnienv->DeleteLocalRef(jmessage);
+
+	//keep parsing events so that the activity is drawn properly
+	int Events = 0;
+	int ident = 0;
+	android_poll_source* source = 0;
+	while(app_global->destroyRequested != 0 && 
+		!error_dialog_returned && ALooper_pollAll(-1, nullptr, &Events, (void**)&source) >= 0) {
+		if(source != NULL)
+			source->process(app_global, source);
+	}
+}
+
 void setTextToClipboard(epro::wstringview text) {
 	jmethodID setClip = jnienv->GetMethodID(nativeActivity, "setClipboard", JPARAMS(JSTRING)JVOID);
 	if(setClip == 0)
@@ -561,7 +592,7 @@ void android_main(android_app *app) {
 	//no longer needed after ndk 15c
 	//app_dummy();
 	retval = main(params.size(), (char**)params.data());
-
 	porting::cleanupAndroid();
+	
 	exit(retval);
 }
