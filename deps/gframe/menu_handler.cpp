4d39b3a6f72274559e21876bc323a73bb8af2d4c
Edoardo Lolletti <edoardo762@gmail.com>
2022-09-19 18:07:55 +0200

    Sync
    https://github.com/edo9300/edopro/commit/4469f2295bdadae9942f2fa78e73d531163d6937

--- deps/gframe/menu_handler.cpp
+++ deps/gframe/menu_handler.cpp
@@ -676,7 +676,7 @@ bool MenuHandler::OnEvent(const irr::SEvent& event) {
 				std::wstring repinfo;
 				time_t curtime = replay.pheader.base.timestamp;
 				repinfo.append(fmt::format(L"{:%Y/%m/%d %H:%M:%S}\n", *std::localtime(&curtime)));
-				auto names = replay.GetPlayerNames();
+				const auto& names = replay.GetPlayerNames();
 				for(int i = 0; i < replay.GetPlayersCount(0); i++) {
 					repinfo.append(names[i] + L"\n");
 				}
@@ -887,7 +887,7 @@ bool MenuHandler::OnEvent(const irr::SEvent& event) {
 				if(curTab == 0) {
 					mainGame->UpdateDuelParam();
 				} else {
-					const auto tcg = mainGame->duel_param & DUEL_TCG_SEGOC_FIRSTTRIGGER;
+					const auto tcg = mainGame->duel_param & DUEL_TCG_SEGOC_NONPUBLIC;
 	#define CHECK(MR) case (MR - 1):{ mainGame->duel_param = DUEL_MODE_MR##MR; mainGame->forbiddentypes = DUEL_MODE_MR##MR##_FORB; break; }
 					switch (mainGame->cbDuelRule->getSelected()) {
 					CHECK(1)
