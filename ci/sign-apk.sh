79de202a166bff2fa11738640b69eb0c541c9ebe
Edoardo Lolletti <edoardo762@gmail.com>
2024-07-18 12:27:38 +0200

    Update default build tools path

--- ci/sign-apk.sh
+++ ci/sign-apk.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 OUT_APK=${1}
 JKS_PASS=${2:-$JKS_PASS}
-BUILD_TOOLS_PATH=${BUILD_TOOLS_PATH:-$ANDROID_HOME/build-tools/29.0.3}
+BUILD_TOOLS_PATH=${BUILD_TOOLS_PATH:-$ANDROID_HOME/build-tools/31.0.0}
 
 "$BUILD_TOOLS_PATH"/zipalign -v -p 4 build/outputs/apk/release/EDOPro-release-unsigned.apk build/outputs/apk/release/EDOPro-release-unsigned-aligned.apk
 mkdir -p release
